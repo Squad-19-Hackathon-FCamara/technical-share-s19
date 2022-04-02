4c0481c5ce75f5b7b7ae4ef0b72719af0744a34e
Joao Lourenco <joaodslourenco@gmail.com>
2022-04-02 14:59:28 -0300

    model de usuário feito

--- node-api/src/models/user.js
+++ node-api/src/models/user.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  name: {},
-  email: {}
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  technologies: {
+    type: String,
+    required: true
+  },
+  job: {
+    type: String,
+    required: true
+  },
+  experience: { type: Number, required: true } // tratar experiência em anos
+  //redes sociais?
 })
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
