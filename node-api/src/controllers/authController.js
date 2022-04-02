66108bbe7996784fbbfbdb817f24a8d06de6e88d
Joao Lourenco <joaodslourenco@gmail.com>
2022-04-02 16:26:16 -0300

    add: rota authenticate

--- node-api/src/controllers/authController.js
+++ node-api/src/controllers/authController.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/auth')
 
 const User = require('../models/user')
 
@@ -19,6 +23,25 @@ router.post('/register', async (req, res) => {
   } catch (error) {
     return res.status(400).send({ error: 'Falha no registro!' })
   }
+})
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user) return res.status(400).send({ error: 'Usuário não encontrado!' })
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).send({ error: 'Senha inválida.' })
+
+  user.password = undefined
+
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 86400
+  })
+
+  res.send({ user, token })
 })
 
 module.exports = app => app.use('/auth', router)
