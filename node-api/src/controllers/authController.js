8a39579cb90c3f8c9a71c1a5d4409e32d6101513
Joao Lourenco <joaodslourenco@gmail.com>
2022-04-02 16:37:30 -0300

    add: geração de token jwt para autenticação

--- node-api/src/controllers/authController.js
+++ node-api/src/controllers/authController.js
@@ -8,6 +8,12 @@ const User = require('../models/user')
 
 const router = express.Router()
 
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400
+  })
+}
+
 router.post('/register', async (req, res) => {
   const { email } = req.body
 
@@ -19,7 +25,7 @@ router.post('/register', async (req, res) => {
 
     user.password = undefined
 
-    return res.send({ user })
+    return res.send({ user, token: generateToken({ id: user.id }) })
   } catch (error) {
     return res.status(400).send({ error: 'Falha no registro!' })
   }
@@ -37,11 +43,7 @@ router.post('/authenticate', async (req, res) => {
 
   user.password = undefined
 
-  const token = jwt.sign({ id: user.id }, authConfig.secret, {
-    expiresIn: 86400
-  })
-
-  res.send({ user, token })
+  res.send({ user, token: generateToken({ id: user.id }) })
 })
 
 module.exports = app => app.use('/auth', router)
