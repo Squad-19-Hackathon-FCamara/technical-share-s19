1ecb32c7b964eee65e32f1c7534b78993bd20b75
Joao Lourenco <joaodslourenco@gmail.com>
2022-04-03 11:05:04 -0300

    add: project controller

--- node-api/src/index.js
+++ node-api/src/index.js
@@ -6,5 +6,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 require('./controllers/authController')(app)
+require('./controllers/projectController')(app)
 
 app.listen(3003)
