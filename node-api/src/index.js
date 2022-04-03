8f1bdef307ae16b3f6f1f6870ff5a884025859b4
Leonardo Falcão <leonardopfalcao1@gmail.com>
2022-04-03 14:58:35 -0300

    add pasta app e centralização das controllers

--- node-api/src/index.js
+++ node-api/src/index.js
@@ -5,7 +5,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-require('./controllers/authController')(app)
-require('./controllers/projectController')(app)
+require('./app/controllers/index')(app)
 
 app.listen(3003)
