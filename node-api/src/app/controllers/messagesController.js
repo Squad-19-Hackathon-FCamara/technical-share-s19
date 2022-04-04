251fe50134558add2102dbf224afd44e0d69c585
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-04-04 19:33:03 -0300

    add: get messages from a chat

--- node-api/src/app/controllers/messagesController.js
+++ node-api/src/app/controllers/messagesController.js
@@ -1,10 +1,9 @@
-const { Router } = require("express");
 const express = require ("express")
 const router = express.Router()
 
 const Message = require("../models/message")
 
-// add
+// add new messages
 
 router.post("/", async (req,res)=>{
     const newMessage = new Message(req.body)
@@ -18,6 +17,17 @@ router.post("/", async (req,res)=>{
 
 })
 
-// get
+// get the messages from a chat
+
+router.get ("/:chatId", async (req,res)=>{
+    try{
+        const messages = await Message.find({
+        chatId: req.params.chatId,
+    })
+    res.status(200).json(messages)
+}catch(err){
+    res.status(500).json(err)
+}
+})
 
 module.exports = app => app.use("/message", router );
