0e57c7936af98f153aaac27481cfe5cd08794a24
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-04-05 21:16:19 -0300

    add: cors e estruturacao de pastas
    Incrementação do CORS e Estruturacao das pastas do src

--- node-api/src/controllers/chatController.js
+++ node-api/src/controllers/chatController.js
@@ -1,4 +1,3 @@
-const { Router } = require("express");
 const express = require ("express")
 const router = express.Router()
 
--- node-api/src/index.js
+++ node-api/src/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
+//Initial config
 
+const express = require('express')
+const cors = require('cors')
 const app = express()
 
+//Initialize cors
+app.use(cors())
+
+// Read Json
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-require('./app/controllers/index')(app)
+require('./controllers/index')(app)
 
+// Inicialize port
 app.listen(3003)
