5fe3766e2a0e71ba5ba26e1c59b20f30e5c35846
Leonardo Falcão <leonardopfalcao1@gmail.com>
2022-04-08 16:57:09 -0300

    troca "/cadastro" para "/register"

---
+++ technical-share-s19/src/pages/Register/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const HeaderTitle = styled.h1`
+  padding-top: 2.813rem;
+`
+
+export const FormLogin = styled.form`
+
+`
+
+export const InputLabel = styled.label`
+
+`
+
+export const InputLogin = styled.input`
+
+`
+
+export const FormBtn = styled.button`
+
+`
+
+export const RedirectCadastro = styled.a`
+
+`
+
+export const SkillsBtn = styled.button`
+
+`
