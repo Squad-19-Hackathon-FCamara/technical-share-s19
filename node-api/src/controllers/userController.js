db21431467a661c039e938cea44f59461711bc61
Leonardo Falcão <leonardopfalcao1@gmail.com>
2022-04-08 15:34:21 -0300

    identação e troca do send() por json()

--- node-api/src/controllers/userController.js
+++ node-api/src/controllers/userController.js
@@ -1,74 +1,68 @@
-
 //Recebe requisicao do routers e solicita no repositorio
 const {getUsers, getSpecificUser,} = require('../repository/userRepository')
 const {createUser, verifyExistingUserByEmail, validateUser, validatePassword} = require('../services/userServices')
 
-
-
 function generateToken(params = {}) {
-    return jwt.sign(params, authConfig.secret, {
-      expiresIn: 86400
-    })
-  }
-
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400
+  })
+}
 
 //Lista todos os Usuarios
 async function listUsers(req,res) {
-    try{
+  try {
     const users = await getUsers()
     return res.send({users})
-    }catch (err) {return res.status(400).send({error:"Erro ao carregar usuarios"})} 
+  } catch (err) {return res.status(400).send({error:"Erro ao carregar usuarios"})} 
 }
+
 //Lista um usuario especifico
 async function listUser (req,res) {
+  try {
+    const user = await getSpecificUser(req)
+    if(user===null ) throw new Error
     
-   try{
-       const user = await getSpecificUser(req)
-       if(user===null ) throw new Error
     return res.send({user})
-   }catch (err) {return res.status(400).send({error:"Erro ao carregar o usuario"})} 
-
+  } catch (err) {return res.status(400).send({error:"Erro ao carregar o usuario"})} 
 }
 
 // Registra um novo usuario
-
 async function registerUser(req,res) {
-    try {
-        const { name, email, cargo, profissao, tags, password } = req.body
+  try {
+    const { name, email, cargo, profissao, tags, password } = req.body
 
-        const user = {
-          name:name,
-          email:email, 
-          cargo:cargo, 
-          profissao:profissao, 
-          tags:tags, 
-          password:password
-        }
+    const user = {
+      name:name,
+      email:email, 
+      cargo:cargo, 
+      profissao:profissao, 
+      tags:tags, 
+      password:password
+    }
+
+    const verifyUser = await verifyExistingUserByEmail(user.email)
     
-        const verifyUser = await verifyExistingUserByEmail(user.email)
-        
-        if (!verifyUser) {
-          const newUser = await createUser(user)
-          return res.status(201).send({ user:newUser })
-        }
-    } catch(error) {
-        return res.status(500).send({ msg: error.message })
+    if (!verifyUser) {
+      const newUser = await createUser(user)
+      return res.status(201).send({ user:newUser })
     }
+  } catch(error) {
+    return res.status(500).send({ msg: error.message })
   }
+}
 
-  // Loga o usuario
+// Loga o usuario
+async function loginUser(req,res) {
+  try {
+    const { email, password } = req.body
 
-  async function loginUser(req,res) {
-      try {
-        const { email, password } = req.body
+    const user = await validateUser(email)
+    const token = await validatePassword(email, password)
+    return res.status(200).json({user:user, token:token})
 
-        const user = await validateUser(email)
-        const token = await validatePassword(email, password)
-        return res.status(201).send({user:user, token:token})
-
-    } catch(error) {
-      return res.status(500).send({ msg: error.message })
-    }
+  } catch(error) {
+    return res.status(401).json({ msg: error.message })
   }
+}
   
 module.exports = {listUsers, listUser, registerUser, loginUser}
